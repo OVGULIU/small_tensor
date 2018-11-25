0bfc11a489d50aade0d41d9a1d9ad9f232b26cb6
Yuan <fengyuan.energy@gmail.com>
2018-11-25 15:19:43 -0800

    added 1D cyclic figure.

--- benchmark/ConcreteDamageModel/plot_sol.py
+++ benchmark/ConcreteDamageModel/plot_sol.py
@@ -1,5 +1,15 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+# #############################
+import matplotlib.pylab as pylab
+params = {'legend.fontsize': 'x-large',
+          'figure.figsize': (10, 8),
+         'axes.labelsize': 'x-large',
+         'axes.titlesize':'x-large',
+         'xtick.labelsize':'x-large',
+         'ytick.labelsize':'x-large'}
+pylab.rcParams.update(params)
+# #############################
 
 data = np.loadtxt('strain_stress.txt')
 
@@ -7,6 +17,10 @@ strain = data[:,0]
 stress = data[:,1]
 
 plt.plot(strain, stress)
+plt.xlabel('Compressive Strain')
+plt.ylabel('Compressive Stress')
+plt.savefig("ConcreteCompressCyclic.jpg")
 plt.show()
+
 
 
