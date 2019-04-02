8ee4310eae4a562f70cc774290d394ca6c84310c
yuan-feng <yuanfeng.energy@gmail.com>
2019-04-01 22:18:04 -0700

    debuging

--- benchmark/vonMises/main.cpp
+++ benchmark/vonMises/main.cpp
@@ -11,20 +11,24 @@ int main(int argc, char const *argv[])
 	outfile.open("strain_stress.txt");
 
 	auto material = std::make_shared<vonMises>() ;
+	material->Initialize();
+
 	tensor2<float,3,3> input_strain ;
 
 	input_strain *= 0.;
-	float strain_incr = 0.001 ; 
+	float strain_incr = 0.0005 ; 
 	input_strain(0,1) = strain_incr;
 	input_strain(1,0) = strain_incr;
 
-	constexpr int Nsteps = 3 ;
+	constexpr int Nsteps = 20 ;
 
+	std::cout << material->_iter_strain(0,1) << " " << material->_iter_stress(0,1) << std::endl; 
+	outfile << material->_iter_strain(0,1) << " " << material->_iter_stress(0,1) << std::endl; 
 	for (int i = 0; i < Nsteps; ++i)	{
 		material->SetTrialStrainIncr(input_strain) ;
 		material->CommitState();
-		std::cout << material->_iter_strain(0,1) << ", " << material->_iter_stress(0,1) << std::endl; 
-		outfile << material->_iter_strain(0,1) << ", " << material->_iter_stress(0,1) << std::endl; 
+		std::cout << material->_iter_strain(0,1) << " " << material->_iter_stress(0,1) << std::endl; 
+		outfile << material->_iter_strain(0,1) << " " << material->_iter_stress(0,1) << std::endl; 
 	}
 
 	outfile.close();
